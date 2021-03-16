1dc28d8c2a9d006a3b62a741850e179feb42f01a
Narek Yazeryan <yazeryann@gmail.com>
2021-03-16 22:12:34 +0400

    Adding font awesome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./font-awesome-pro.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Wrapper">
+            <header>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.g4f-records.com%2Fen%2Fqui-sommes-nous%2Flogo-facebook-2%2F&psig=AOvVaw3BNSIfozgr06zMGLFNR2UT&ust=1616004292154000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCE1qSzte8CFQAAAAAdAAAAABAK" />
+            </header>
+
+            <nav className="sidebar">
+                <ul className="menuList">
+                    <li className="menuItem">
+                        <i class="fal fa-user-circle menuItem-icon"></i>
+
+						<a href="#s">My Profile</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header () {
+    return(
+        <div className="Header">
+            <a href="#s">Home</a>
+            <a href="#s">About</a>
+            <a href="#s">Services</a>
+            <a href="#s">Contact</a>
+        </div>
+    )
+}
